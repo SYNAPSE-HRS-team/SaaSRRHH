6a8119a2ff75fb57a3961cc64e77d235a6644fa0
Miguel Alejandro <U23200683@utp.edu.pe>
2026-04-18 23:45:30 -0500

    Creación del model AccesoUsuario

--- src/main/java/com/SaasRRHH/main/model/AccesoUsuario.java
+++ src/main/java/com/SaasRRHH/main/model/AccesoUsuario.java
@@ -6,9 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
-@Data
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
