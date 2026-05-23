0e0c181d9782438c3d692a11446f38bd88e29f90
Ricardo Hernandez <u23273444@utp.edu.pe>
2026-05-23 00:02:19 -0500

    correcion conflicto

--- src/main/java/com/SaasRRHH/main/controller/FamiliarController.java
+++ src/main/java/com/SaasRRHH/main/controller/FamiliarController.java
@@ -4,26 +4,14 @@ import com.SaasRRHH.main.model.Familiar;
 import com.SaasRRHH.main.services.FamiliarService;
 
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 
-import java.util.List;
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/familiares")
-public class FamiliarController {
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/familiares")
@@ -87,9 +75,8 @@ public class FamiliarController {
 
         } catch (RuntimeException e) {
 
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.notFound().build(); 
         }
     }
-}
 }
 
